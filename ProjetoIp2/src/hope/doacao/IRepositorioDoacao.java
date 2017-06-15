283bc4fa26903d140c1a85c5662ac8cc0b598ec8
Felipe9 <felipe.cavalcanty@hotmail.com>
2017-06-15 18:45:02 -0300

    Interface RepositorioDoacao e add metodos na Fachada

--- ProjetoIp2/src/hope/doacao/IRepositorioDoacao.java
+++ ProjetoIp2/src/hope/doacao/IRepositorioDoacao.java
@@ -2,4 +2,13 @@ package hope.doacao;
 
 public interface IRepositorioDoacao {
 
+	public boolean cadastrarDoacao(Doacao doacao);
+	
+	public Doacao buscarDoacao(int codigoDoacao);
+	
+	public boolean atualizarDoacao(Doacao novaDoacao);
+	
+	public boolean removerDoacao(int codigoDoacao);
+	
+	public String listarDoacoes();
 }
