9eda8d2a7a03905a21d5e52da564a98a747cde91
Tássia Bastos <tassinha.cpr@gmail.com>
2017-06-20 18:38:49 -0300

    Add atributos a classe Higiene

--- ProjetoIp2/src/hope/produto/Higiene.java
+++ ProjetoIp2/src/hope/produto/Higiene.java
@@ -2,11 +2,15 @@ package hope.produto;
 
 public class Higiene extends Produto {
 	private String descricao;
+	private boolean itemLimpPessoal;
+	private boolean itemLimpGeral;
 
 	public Higiene(String tipo, int quantidade, String nome, int codigo,
-			String descricao) {
+			String descricao, boolean itemLimpPessoal, boolean itemLimpGeral) {
 		super(tipo, quantidade, nome, codigo);
 		this.descricao = descricao;
+		this.itemLimpPessoal = itemLimpPessoal;
+		this.itemLimpGeral = itemLimpGeral;
 	}
 
 	public String getDescricao() {
@@ -16,6 +20,21 @@ public class Higiene extends Produto {
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
-	
+
+	public boolean isItemLimpPessoal() {
+		return itemLimpPessoal;
+	}
+
+	public void setItemLimpPessoal(boolean itemLimpPessoal) {
+		this.itemLimpPessoal = itemLimpPessoal;
+	}
+
+	public boolean isItemLimpGeral() {
+		return itemLimpGeral;
+	}
+
+	public void setItemLimpGeral(boolean itemLimpGeral) {
+		this.itemLimpGeral = itemLimpGeral;
+	}
 	
 }
