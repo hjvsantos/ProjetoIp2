a69fb8b068306edf0e47b7090dc5865b0a39240f
Tássia Bastos <tassinha.cpr@gmail.com>
2017-08-09 20:49:05 -0300

    GUI - .java - inclusão dos botões

--- ProjetoIp2/src/gui/Tela_Menu_InicialController.java
+++ ProjetoIp2/src/gui/Tela_Menu_InicialController.java
@@ -1,5 +1,24 @@
 package gui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class Tela_Menu_InicialController {
+	
+	@FXML
+	private Button menuLogar;
+	@FXML
+	private Button menuCadastrar;
+	
+	@FXML
+	private Button BT_SAIR;
+	
+	
+	
 
 }
