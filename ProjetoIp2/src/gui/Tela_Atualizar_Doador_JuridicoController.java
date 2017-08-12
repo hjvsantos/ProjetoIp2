1dd96d2860226599894a5b91dad57db2b8abb77f
João Lucas Santos <joao__lucas_santos@hotmail.com>
2017-08-12 15:59:36 -0300

    Botões cancelar das classes de atualização

--- ProjetoIp2/src/gui/Tela_Atualizar_Doador_JuridicoController.java
+++ ProjetoIp2/src/gui/Tela_Atualizar_Doador_JuridicoController.java
@@ -1,5 +1,38 @@
 package gui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class Tela_Atualizar_Doador_JuridicoController {
 
+	@FXML
+	private Button bt_atualizar;
+	
+	@FXML
+	private Button bt_cancelar;
+	
+	public void Cancelar(ActionEvent event) {
+		Parent root;
+		Stage stage;
+		try {
+			if (event.getSource() == bt_cancelar) {
+				stage = (Stage) bt_cancelar.getScene().getWindow();
+				root = FXMLLoader.load(
+						getClass().getResource("/ProjetoIp2/src/gui/Tela_Principal_Doador_Juridico.fxml"));
+			} else {
+				stage = (Stage) bt_cancelar.getScene().getWindow();
+				root = FXMLLoader.load(getClass().getResource("/ProjetoIp2/src/gui/Tela_Atualizar_Doador_Juridico.fxml"));
+			}
+			Scene scene = new Scene(root);
+			stage.setScene(scene);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
--- ProjetoIp2/src/gui/Tela_Atualizar_InstituicaoController.java
+++ ProjetoIp2/src/gui/Tela_Atualizar_InstituicaoController.java
@@ -1,5 +1,38 @@
 package gui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class Tela_Atualizar_InstituicaoController {
 
+	@FXML
+	private Button bt_atualizar;
+	
+	@FXML
+	private Button bt_cancelar;
+	
+	public void Cancelar(ActionEvent event) {
+		Parent root;
+		Stage stage;
+		try {
+			if (event.getSource() == bt_cancelar) {
+				stage = (Stage) bt_cancelar.getScene().getWindow();
+				root = FXMLLoader.load(
+						getClass().getResource("/ProjetoIp2/src/gui/Tela_Principal_Instituicao.fxml"));
+			} else {
+				stage = (Stage) bt_cancelar.getScene().getWindow();
+				root = FXMLLoader.load(getClass().getResource("/ProjetoIp2/src/gui/Tela_Atualizar_Instituicao.fxml"));
+			}
+			Scene scene = new Scene(root);
+			stage.setScene(scene);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
