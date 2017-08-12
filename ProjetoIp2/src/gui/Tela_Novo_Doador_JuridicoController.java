8d597493f047733e7f2373e5b789edf667078dd3
Tássia Bastos <tassinha.cpr@gmail.com>
2017-08-12 16:04:13 -0300

    Implementação da Tela_Novo_Doador_JuridicoController

--- ProjetoIp2/src/gui/Tela_Novo_Doador_JuridicoController.java
+++ ProjetoIp2/src/gui/Tela_Novo_Doador_JuridicoController.java
@@ -1,5 +1,59 @@
 package gui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class Tela_Novo_Doador_JuridicoController {
+	
+	@FXML
+	private Button bt_proximo;
+	
+	@FXML
+	private Button bt_voltar;
+	
+	public void Proximo(ActionEvent event) {
+		Parent root;
+		Stage stage;
+		try {
+			if (event.getSource() == bt_proximo) {
+				stage = (Stage) bt_proximo.getScene().getWindow();
+				root = FXMLLoader.load(
+						getClass().getResource("/ProjetoIp2/src/gui/Tela_Principal_Doador_Juridico.fxml"));
+			} else {
+				stage = (Stage) bt_proximo.getScene().getWindow();
+				root = FXMLLoader.load(getClass().getResource("/ProjetoIp2/src/gui/Tela_Novo_Doador_Juridico.fxml"));
+			}
+			Scene scene = new Scene(root);
+			stage.setScene(scene);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void Voltar(ActionEvent event) {
+		Parent root;
+		Stage stage;
+		try {
+			if (event.getSource() == bt_voltar) {
+				stage = (Stage) bt_voltar.getScene().getWindow();
+				root = FXMLLoader.load(
+						getClass().getResource("/ProjetoIp2/src/gui/Tela_Menu_Inicial.fxml"));
+			} else {
+				stage = (Stage) bt_voltar.getScene().getWindow();
+				root = FXMLLoader.load(getClass().getResource("/ProjetoIp2/src/gui/Tela_Novo_Doador_Juridico.fxml"));
+			}
+			Scene scene = new Scene(root);
+			stage.setScene(scene);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 }
