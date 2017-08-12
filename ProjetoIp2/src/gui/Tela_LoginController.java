bacc291e56d56928c0a95a26e7f0fc8bbb7d1534
João Lucas Santos <joao__lucas_santos@hotmail.com>
2017-08-12 13:44:14 -0300

    Botão Voltar da tela Login

--- ProjetoIp2/src/gui/Tela_LoginController.java
+++ ProjetoIp2/src/gui/Tela_LoginController.java
@@ -1,5 +1,38 @@
 package gui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class Tela_LoginController {
 
+	@FXML
+	private Button bt_entrar;
+	@FXML
+	private Button bt_voltar;
+	
+	public void botaoVoltar(ActionEvent event) {
+		Parent root;
+		Stage stage;
+		try {
+			if (event.getSource() == bt_voltar) {
+				stage = (Stage) bt_voltar.getScene().getWindow();
+				root = FXMLLoader.load(
+						getClass().getResource("/ProjetoIp2/src/gui/Tela_Menu_Inicial.fxml"));
+			} else {
+				stage = (Stage) bt_voltar.getScene().getWindow();
+				root = FXMLLoader.load(getClass().getResource("/ProjetoIp2/src/gui/Tela_Login.fxml"));
+			}
+			Scene scene = new Scene(root);
+			stage.setScene(scene);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
