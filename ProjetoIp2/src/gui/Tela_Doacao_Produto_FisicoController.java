eb77b596a284e87af28cd73e2985d6298ff5555d
João Lucas Santos <joao__lucas_santos@hotmail.com>
2017-08-12 16:25:10 -0300

    botoes cancelar das delas de doação de produtos

---
+++ ProjetoIp2/src/gui/Tela_Doacao_Produto_FisicoController.java
@@ -0,0 +1,37 @@
+package gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class Tela_Doacao_Produto_FisicoController {
+	@FXML
+	private Button bt_doar;
+	
+	@FXML
+	private Button bt_cancelar;
+	
+	public void Cancelar(ActionEvent event) {
+		Parent root;
+		Stage stage;
+		try {
+			if (event.getSource() == bt_cancelar) {
+				stage = (Stage) bt_cancelar.getScene().getWindow();
+				root = FXMLLoader.load(
+						getClass().getResource("/ProjetoIp2/src/gui/Tela_Principal_Doador_Fisico.fxml"));
+			} else {
+				stage = (Stage) bt_cancelar.getScene().getWindow();
+				root = FXMLLoader.load(getClass().getResource("/ProjetoIp2/src/gui/Tela_Doacao_Produto_Fisico.fxml"));
+			}
+			Scene scene = new Scene(root);
+			stage.setScene(scene);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
