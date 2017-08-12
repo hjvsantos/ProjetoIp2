034b634a9d4e21255320d06eb2a863f5e75a7223
João Lucas Santos <joao__lucas_santos@hotmail.com>
2017-08-12 13:34:53 -0300

    .java da tela inicial

--- ProjetoIp2/src/gui/Tela_Menu_InicialController.java
+++ ProjetoIp2/src/gui/Tela_Menu_InicialController.java
@@ -1,5 +1,7 @@
 package gui;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -19,6 +21,49 @@ public class Tela_Menu_InicialController {
 	private Button BT_SAIR;
 	
 	
+	private void fecharTelaAction(){
+	    Stage stage = (Stage) BT_SAIR.getScene().getWindow(); 
+	    stage.close(); 
+	}
 	
+	public void AbrirLogin(ActionEvent event) {
+		Parent root;
+		Stage stage;
+		try {
+			if (event.getSource() == menuLogar) {
+				stage = (Stage) menuLogar.getScene().getWindow();
+				root = FXMLLoader.load(
+						getClass().getResource("/ProjetoIp2/src/gui/Tela_Login.fxml"));
+			} else {
+				stage = (Stage) menuLogar.getScene().getWindow();
+				root = FXMLLoader.load(getClass().getResource("/ProjetoIp2/src/gui/Tela_Menu_Inicial.fxml"));
+			}
+			Scene scene = new Scene(root);
+			stage.setScene(scene);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void AbrirCadastro(ActionEvent event) {
+		Parent root;
+		Stage stage;
+		try {
+			if (event.getSource() == menuCadastrar) {
+				stage = (Stage) menuCadastrar.getScene().getWindow();
+				root = FXMLLoader.load(
+						getClass().getResource("/ProjetoIp2/src/gui/Tela_Novo_Cadastro_Geral.fxml"));
+			} else {
+				stage = (Stage) menuCadastrar.getScene().getWindow();
+				root = FXMLLoader.load(getClass().getResource("/ProjetoIp2/src/gui/Tela_Menu_Inicial.fxml"));
+			}
+			Scene scene = new Scene(root);
+			stage.setScene(scene);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 }
