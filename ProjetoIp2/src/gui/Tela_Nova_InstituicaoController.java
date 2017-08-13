1f84fe3949cf39e27df4c91e17ab3360338c369e
João Lucas Santos <joao__lucas_santos@hotmail.com>
2017-08-13 19:22:59 -0300

    Finalização da primeira parte da implementação dos botões

--- ProjetoIp2/src/gui/Tela_Nova_InstituicaoController.java
+++ ProjetoIp2/src/gui/Tela_Nova_InstituicaoController.java
@@ -1,5 +1,38 @@
 package gui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class Tela_Nova_InstituicaoController {
 
+	@FXML
+	private Button bt_cadastrar;
+	
+	@FXML
+	private Button bt_cancelar;
+	
+	public void Cancelar(ActionEvent event) {
+		Parent root;
+		Stage stage;
+		try {
+			if (event.getSource() == bt_cancelar) {
+				stage = (Stage) bt_cancelar.getScene().getWindow();
+				root = FXMLLoader.load(
+						getClass().getResource("/ProjetoIp2/src/gui/Tela_Novo_Cadastro_Geral.fxml"));
+			} else {
+				stage = (Stage) bt_cancelar.getScene().getWindow();
+				root = FXMLLoader.load(getClass().getResource("/ProjetoIp2/src/gui/Tela_Nova_Instituicao.fxml"));
+			}
+			Scene scene = new Scene(root);
+			stage.setScene(scene);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
--- ProjetoIp2/src/gui/Tela_Remover_UsuariosController.java
+++ ProjetoIp2/src/gui/Tela_Remover_UsuariosController.java
@@ -1,5 +1,37 @@
 package gui;
 
-public class Tela_Remover_UsuariosController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
+public class Tela_Remover_UsuariosController {
+	@FXML
+	private Button bt_deletar;
+	
+	@FXML
+	private Button bt_cancelar;
+	
+	public void Cancelar(ActionEvent event) {
+		Parent root;
+		Stage stage;
+		try {
+			if (event.getSource() == bt_cancelar) {
+				stage = (Stage) bt_cancelar.getScene().getWindow();
+				root = FXMLLoader.load(
+						getClass().getResource("/ProjetoIp2/src/gui/Tela_Principal_Adm.fxml"));
+			} else {
+				stage = (Stage) bt_cancelar.getScene().getWindow();
+				root = FXMLLoader.load(getClass().getResource("/ProjetoIp2/src/gui/Tela_Remover_Usuarios.fxml"));
+			}
+			Scene scene = new Scene(root);
+			stage.setScene(scene);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
