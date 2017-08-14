dd2ed889affee321923a2d25b0d1c24e4badb8e6
Tássia Bastos <tassinha.cpr@gmail.com>
2017-08-14 08:28:19 -0300

    Ajustes na Tela de Login controller

--- ProjetoIp2/src/gui/Tela_LoginController.java
+++ ProjetoIp2/src/gui/Tela_LoginController.java
@@ -1,20 +1,71 @@
 package gui;
 
+
+import aplicacao.Fachada;
+import hope.excecao.ErroDeNegocioExcecao;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class Tela_LoginController {
+	
+	//private Principal principal;
+	Fachada fachada = Fachada.getInstance();
 
+	@FXML
+	private TextField tl_tf_usuario;
+	@FXML
+	private PasswordField tl_pf_senhaUsuario;
 	@FXML
 	private Button bt_entrar;
 	@FXML
 	private Button bt_voltar;
 	
+	/*
+	public void setPrincipal(Principal principal){
+		this.principal = principal;
+	}
+	
+	
+	public void acessarLogin() throws ErroDeNegocioExcecao{
+		this.setPrincipal(principal.getInstance());
+	}
+	
+	@FXML
+	public void acessarLogin(ActionEvent event) throws ErroDeNegocioExcecao {
+
+		try {
+			 if (this.fachada.efetuarLogin(tl_tf_usuario.getText(), tl_pf_senhaUsuario.getText()) == 1) {
+				Parent root;
+				Stage stage;
+
+				stage = (Stage) TL_BT_Acessar.getScene().getWindow();
+				root = FXMLLoader.load(getClass().getResource("/br/ufrpe/sistema_mercadinho/gui/Backoffice.fxml"));
+				Scene scene = new Scene(root);
+				stage.setScene(scene);
+			}else if (this.fachada.efetuarLogin(TL_TF_Usuario.getText(), TL_PF_SenhaLogin.getText()) == -1) {
+				Parent root;
+				Stage stage;
+
+				stage = (Stage) TL_BT_Acessar.getScene().getWindow();
+				root = FXMLLoader.load(getClass().getResource("/br/ufrpe/sistema_mercadinho/gui/FrenteDeCaixa.fxml"));
+				Scene scene = new Scene(root);
+				stage.setScene(scene);
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	*/
 	public void botaoVoltar(ActionEvent event) {
 		Parent root;
 		Stage stage;
