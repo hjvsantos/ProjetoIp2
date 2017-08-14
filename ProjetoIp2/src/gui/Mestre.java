fc87d5cc3ee5e470033733ed6a9f9390dfcb1d44
Tássia Bastos <tassinha.cpr@gmail.com>
2017-08-14 18:25:52 -0300

    Implementação da classe mestre

--- ProjetoIp2/src/gui/Mestre.java
+++ ProjetoIp2/src/gui/Mestre.java
@@ -1,10 +1,113 @@
 package gui;
 
-public class Mestre {
+import java.io.IOException;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import hope.administrador.Adm;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
+public class Mestre extends Application {
+	
+	private Adm acesso = null;
+	private Stage primaryStage;
+	private BorderPane rootLayout;
+	private static Mestre instancia;
+	
+	public static Mestre getInstance(){
+		if (instancia == null) {
+			instancia = new Mestre();
+		}
+		return instancia;
 	}
 
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("HOPE");
+		this.primaryStage.setResizable(false);
+		
+		initRootLayout();
+		if(acesso == null){
+			showLogin();
+		}
+	}
+	
+	private void initRootLayout(){
+		try{
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Mestre.class.getResource("RootLayout.fxml"));
+			rootLayout = (BorderPane) loader.load();
+			
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void showLogin(){
+		try{
+			FXMLLoader loader = new FXMLLoader();
+			//TODO conferir se o path ta correto!!
+			loader.setLocation(Mestre.class.getResource("/hope/gui/Tela_Login.fxml"));
+			AnchorPane Login = (AnchorPane) loader.load();
+
+			rootLayout.setCenter(Login);
+			Tela_LoginController controller = loader.getController();
+			controller.setMestre(this);
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+	}
+	
+	public void Tela_Menu_Inicial(){
+		try{
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Mestre.class.getResource("/hope/gui/Tela_Menu_Inicial.fxml"));
+			AnchorPane Login = (AnchorPane) loader.load();
+			//TODO tem que ver se o da gnt ta no anchorPane tb
+			
+			rootLayout.setCenter(Login);
+			Tela_LoginController controller = loader.getController();
+			controller.setMestre(this);
+			
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+	}
+
+	public void Tela_Login() {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Mestre.class.getResource("/hope/gui/Tela_Login.fxml"));
+			AnchorPane Login = (AnchorPane) loader.load();
+
+			rootLayout.setCenter(Login);
+			Tela_LoginController controller = loader.getController();
+			controller.setMestre(this);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void cadastroAdministrador() {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Mestre.class.getResource("/hope/gui/Tela_Novo_Adm.fxml"));
+			AnchorPane Login = (AnchorPane) loader.load();
+
+			rootLayout.setCenter(Login);
+			Tela_LoginController controller = loader.getController();
+			controller.setMestre(this);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
