cb2c63b22f23005edeb5d44e7ed1835368d98e17
Felipe9 <felipe.cavalcanty@hotmail.com>
2017-08-15 00:01:08 -0300

    Adição do cadastrar no Controller de NovoDoadorFísico

--- ProjetoIp2/src/gui/Tela_Novo_Doador_FisicoController.java
+++ ProjetoIp2/src/gui/Tela_Novo_Doador_FisicoController.java
@@ -2,19 +2,24 @@ package gui;
 
 import java.awt.TextField;
 
+import hope.doador.ControladorDoadorPessoa;
+import hope.doador.DoadorPessoa;
+import hope.excecao.ErroDeNegocioExcecao;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 
 public class Tela_Novo_Doador_FisicoController {
 	
 	@FXML
-	private Button bt_proximo;
+	private Button bt_;
 	
 	@FXML
 	private Button bt_voltar;
@@ -34,24 +39,77 @@ public class Tela_Novo_Doador_FisicoController {
 	@FXML
 	private TextField tx_estado;
 	
+private ControladorDoadorPessoa cdp;
 	
-	void Proximo(ActionEvent event) {
-		Parent root;
-		Stage stage;
-		try {
-			if (event.getSource() == bt_proximo) {
-				stage = (Stage) bt_proximo.getScene().getWindow();
-				root = FXMLLoader.load(
-						getClass().getResource("/ProjetoIp2/src/gui/Tela_Principal_Doador_Fisico.fxml"));
-			} else {
-				stage = (Stage) bt_proximo.getScene().getWindow();
-				root = FXMLLoader.load(getClass().getResource("/ProjetoIp2/src/gui/Tela_Novo_Doador_Fisico.fxml"));
-			}
-			Scene scene = new Scene(root);
-			stage.setScene(scene);
+	@FXML private TextField tf_Nome_DoadorFisico;
+	@FXML private Integer tf_Idade_DoadorFisico;
+	@FXML private TextField tf_CPF_DoadorFisico;
+	@FXML private Double tf_RG_DoadorFisico;
+	@FXML private TextField tf_Cidade_DoadorFisico;
+	@FXML private TextField tf_Estado_DoadorFisico;
+	@FXML private Double tf_NumeroCartao_DoadorFisico;
+	@FXML private Integer tf_CodigoCard_DoadorFisico;
+	@FXML private TextField tf_CodDoador_DoadorFisico;
+	@FXML private TextField tf_bandeiraCard_DoadorFisico;
+	@FXML private TextField tf_ValidadeCard_DoadorFisico;
+	@FXML private PasswordField ps_Senha_DoadorFisico;
 
-		} catch (Exception e) {
-			e.printStackTrace();
+	@FXML
+	private void limparFields() {
+		tf_Nome_DoadorFisico.setText(" ");
+		tf_CPF_DoadorFisico.setText(" ");
+		tf_Idade_DoadorFisico.intValue();
+		tf_RG_DoadorFisico.doubleValue();
+		tf_Cidade_DoadorFisico.setText(" ");
+		tf_Estado_DoadorFisico.setText(" ");
+		tf_NumeroCartao_DoadorFisico.doubleValue();
+		tf_CodigoCard_DoadorFisico.intValue();
+		tf_CodDoador_DoadorFisico.setText(" ");
+		tf_bandeiraCard_DoadorFisico.setText(" ");
+		tf_ValidadeCard_DoadorFisico.setText(" ");
+		ps_Senha_DoadorFisico.setText(" ");
+		
+	}
+	
+	@FXML 
+	private void cadastrarDoadorFisico() {
+		cdp = ControladorDoadorPessoa.getInstance();
+		String nome = tf_Nome_DoadorFisico.getText();
+		String cpf = tf_CPF_DoadorFisico.getText();
+		Integer idade = tf_Idade_DoadorFisico.getInteger(nome, tf_Idade_DoadorFisico); 
+		Double RG = tf_RG_DoadorFisico.doubleValue(); 
+		String cidade = tf_Cidade_DoadorFisico.getText();
+		String estado = tf_Estado_DoadorFisico.getText();
+		Double numCard = tf_NumeroCartao_DoadorFisico.doubleValue();
+		Integer codCard = tf_CodigoCard_DoadorFisico.getInteger(cpf, tf_CodigoCard_DoadorFisico);
+		String codDoador = tf_CodDoador_DoadorFisico.getText();
+		String bandeira = tf_bandeiraCard_DoadorFisico.getText();
+		String validade = tf_ValidadeCard_DoadorFisico.getText();
+		String senha = ps_Senha_DoadorFisico.getText();
+		if((nome != null && nome != " ") && (cpf != null && cpf != " ") 
+				&& (idade != null) && (RG != null)
+				&& (cidade != null && cidade != " ")
+				&& (estado != null && estado != " ")
+				&& (numCard != null)
+				&& (codCard != null)
+				&& (codDoador != null)
+				&& (bandeira != null && bandeira != " ")
+				&& (validade != null && validade != " ")
+				&& (senha != null && senha != " ")) {
+			DoadorPessoa p = new DoadorPessoa(nome,idade,cpf,RG,cidade,estado,numCard,codCard,codDoador, bandeira, validade, senha);
+			try {
+				cdp.cadastrarDoador(p);
+			} catch (ErroDeNegocioExcecao e) {
+				e.printStackTrace();
+			}
+			limparFields();
+			Alert alerta = new Alert(AlertType.CONFIRMATION);
+   		 	alerta.setTitle("Cadastro realizado com sucesso!");
+		}
+		else
+		{
+			Alert alerta = new Alert(AlertType.ERROR);
+   		 	alerta.setTitle("ERRO!");
 		}
 	}
 	
