fe7c8d324e040ad7dc89de16d8b7298d61a2ece2
João Lucas Santos <joao__lucas_santos@hotmail.com>
2017-08-15 21:34:50 -0300

    Teste no cadastro de novo doador juridico

--- ProjetoIp2/src/gui/Tela_Novo_Doador_JuridicoController.java
+++ ProjetoIp2/src/gui/Tela_Novo_Doador_JuridicoController.java
@@ -1,7 +1,16 @@
 package gui;
 
-import java.awt.TextField;
+import hope.doador.DoadorEmpresa;
+import hope.instituicao.Instituicao;
 
+import java.awt.TextField;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+import aplicacao.Fachada;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -37,8 +46,90 @@ public class Tela_Novo_Doador_JuridicoController {
 	@FXML
 	private TextField tx_ano_fundacao;
 	
-	public void Cadastro(ActionEvent event) {
+	private TextField tx_ncartao;
+	
+	@FXML
+	private TextField tx_codcartao;
+	
+	@FXML 
+	private TextField tx_bandeira;
+	
+	@FXML 
+	private TextField tx_validade;
+	
+	
+	Fachada fachada;
+	
+	public void initialize(){
+		fachada = fachada.getInstance();
+	}
+	
+	public void Cadastro(ActionEvent event) throws IOException {
+		Parent root;
+		Stage stage;
 		
+		if(validarCampos()){
+		
+			try {
+				String nome, cnpj, cidade, estado, ano, numerocartao, senha, bandeira, codigo, validade;
+				int codUsuario;
+				
+				nome = tx_nome.getText();
+				cnpj = tx_cnpj.getText();
+				cidade = tx_cidade.getText();
+				estado = tx_estado.getText();
+				ano	= tx_ano_fundacao.getText();
+				numerocartao = tx_ncartao.getText();
+				senha = ps_senha.getText();
+				bandeira = tx_bandeira.getText();
+				codigo = tx_codcartao.getText();
+				validade = tx_validade.getText();
+				
+				
+				DoadorEmpresa doador = new DoadorEmpresa(nome, 0, cnpj, 0, 0, cidade, estado, numerocartao, codigo, bandeira, validade, senha);
+				fachada.cadastrarDoadorEmpresa(doador);
+								
+				
+				
+				stage = (Stage) bt_proximo.getScene().getWindow();
+				root = FXMLLoader.load(getClass().getResource("/ProjetoIp2/src/gui/Tela_Login.fxml"));
+				
+				Scene scene = new Scene(root);
+				stage.setScene(scene);
+				
+		} catch (Exception e){
+			Alert alert = new Alert(AlertType.WARNING);
+			alert.setTitle("Error");
+			alert.setHeaderText("Informações inválidas");
+			alert.setContentText(e.getMessage());
+			alert.showAndWait();
+		}
+	}
+	}
+	
+	private boolean validarCampos() throws IOException {
+		boolean validade = false;
+		try {
+			if (tx_ano_fundacao.getText().isEmpty() || tx_cnpj.getText().isEmpty()
+					|| tx_cidade.getText().isEmpty() || tx_estado.getText().isEmpty()
+					|| tx_codcartao.getText().isEmpty() || tx_bandeira.getText().isEmpty() || tx_ncartao.getText().isEmpty()  || tx_validade.getText().isEmpty()) {
+				Alert alert = new Alert(AlertType.WARNING);
+				alert.setTitle("Error");
+				alert.setHeaderText("Informações inválidas");
+				alert.setContentText("Verifique os campos digitados!");
+				alert.showAndWait();
+
+			} else {
+				validade = true;
+			}
+		} catch (NumberFormatException e) {
+			Alert alert = new Alert(AlertType.WARNING);
+			alert.setTitle("Error");
+			alert.setHeaderText("Informações inválidas");
+			alert.setContentText("Verifique os campos digitados!");
+			alert.showAndWait();
+		}
+		return validade;
 	}
 	
 	public void Voltar(ActionEvent event) {
